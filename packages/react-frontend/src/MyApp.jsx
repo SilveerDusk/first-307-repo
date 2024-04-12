0eefcefc2f93f227ca9f0b236eb1cb4038968aa5
Jason <Jason1@jelincic.com>
2024-04-11 17:33:55 -0700

    feat: connects backend to frontend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,14 +1,26 @@
 // src/MyApp.jsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from './Table';
 import Form from './Form';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+  
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, [] );
 
   function updateList(person) {
     setCharacters([...characters, person]);
-  }  
+  }
+
+  function fetchUsers() {
+    const promise = fetch('http://localhost:8000/users');
+    return promise;
+  }
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
