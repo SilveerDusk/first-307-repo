e8539358b1938b652c5f92e8722979560b9e0fda
Jason <Jason1@jelincic.com>
2024-04-11 17:38:44 -0700

    feat: connects post request from frontend to backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -13,8 +13,24 @@ function MyApp() {
       .catch((error) => { console.log(error); });
   }, [] );
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   function fetchUsers() {
